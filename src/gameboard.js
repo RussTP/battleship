90d46cad662f4a7093aa44ef2cde47e851d7199d
RussTP <rpawlett@live.com>
2026-02-26 15:13:59 -0500

    created placeShip function and tdd to place ships horizontally and vertically on gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,18 @@
+import Ship from "./ship";
+
 class Gameboard {
     constructor() {
         this.rows = 10;
         this.cols = 10;
         this.grid = this.createBoard();
+        this.placement = [];
+
+        this.ship = {
+            carrier: new Ship(5),
+            battleship: new Ship(4),
+            submarine: new Ship(3),
+            destroyer: new Ship(2)
+        }
     }
 
     createBoard() {
@@ -16,6 +26,21 @@ class Gameboard {
      }
      return grid; 
     }
+
+    placeShip(name, row, col, direction) {
+        const ship = this.ship[name];
+        const cells = [];
+    for (let i = 0; i < ship.length; i++) {
+        if (direction  === 'horizontal') {
+             cells.push([row, col + i])
+        } else {
+             cells.push([row + i, col])
+        }
+    }
+        this.placement.push({ ship: name, cells: cells })
+        return this.placement
+ }
+
     
     receiveAttack() {
 
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -6,6 +6,9 @@ test('Create gameboard 10x10 grid', () => {
     expect(board.grid[0].length).toBe(10);
 })
 
-test('Receive attack on specified grid cooridnates', () => {
-    
+test('place ships on the board', () => {
+    const board = new Gameboard();
+    board.placeShip('carrier', 0, 0, 'horizontal');
+    expect(board.placement).toEqual([{ ship: 'carrier', cells: [[0,0],[0,1],[0,2],[0,3],[0,4]] }])
 })
+
